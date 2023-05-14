16511c98808679ab5a311aa659fbfea20cc94c73
Thomas Smith <thsm@umich.edu>
2023-05-14 00:02:53 -0400

    Improve get_recipe API stub

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,5 @@
 use axum::{
+    extract::Path,
     routing::get,
     Router,
 };
@@ -7,8 +8,8 @@ async fn list_recipes() -> &'static str {
     "List recipes not yet implemented"
 }
 
-async fn get_recipe() -> &'static str {
-    "Get recipe not yet implemented"
+async fn get_recipe(Path(recipe_id): Path<u64>) -> String {
+    format!("Get recipe (id = {}) not yet implemented", recipe_id)
 }
 
 pub fn create_router() -> Router {
