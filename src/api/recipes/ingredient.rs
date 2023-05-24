7c14b1d7cdc0915d371ca6182c61a1065d083d5b
Thomas Smith <thsm@umich.edu>
2023-05-23 23:58:47 -0400

    Use RealQuantity typedef in ingredient.rs

--- src/api/recipes/ingredient.rs
+++ src/api/recipes/ingredient.rs
@@ -1,15 +1,17 @@
 use serde::Serialize;
 
+type RealQuantity = f64; // TODO: use a better type for this
+
 #[derive(Serialize)]
 pub struct Ingredient {
     pub id: u64,
     pub name: String,
-    pub energy_density: f64, // In J/kg. TODO: use a better type
+    pub energy_density: RealQuantity, // In J/kg.
 }
 
 #[derive(Serialize)]
 pub struct Amount {
-    pub quantity: f64, // In SI standard units. TODO: use a better type.
+    pub quantity: RealQuantity, // In SI standard units.
     pub measurement: String,
 }
 
