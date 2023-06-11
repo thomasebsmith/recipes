47de8ca23b1e490d88c45914d7bb7a41c548780c
Thomas Smith <thsm@umich.edu>
2023-06-11 00:06:58 -0400

    Add config file parsing

---
+++ src/config.rs
@@ -0,0 +1,40 @@
+use serde::Deserialize;
+use std::net::IpAddr;
+use std::{env, fs};
+
+#[derive(Deserialize)]
+pub struct DatabaseConfig {
+    #[serde(rename = "type")]
+    pub db_type: String,
+    pub connection_url: String,
+}
+
+#[derive(Deserialize)]
+pub struct ServerConfig {
+    pub ip_address: IpAddr,
+    pub port: u16,
+}
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub database: DatabaseConfig,
+    pub server: ServerConfig,
+}
+
+pub fn get_config() -> Result<Config, String> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        let executable_name =
+            if args.is_empty() { "recipes" } else { &args[0] };
+        Err(format!("Usage: {} <config file>", executable_name))
+    } else {
+        let config_file_contents = stringify_err(fs::read_to_string(&args[1]))?;
+        let config = stringify_err(toml::from_str(&config_file_contents))?;
+        Ok(config)
+    }
+}
+
+fn stringify_err<T, U: ToString>(result: Result<T, U>) -> Result<T, String> {
+    result.map_err(|err| err.to_string())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,25 @@
 mod api;
+mod config;
 
 use axum::{routing::get, Router};
-use std::env;
-use std::net::{IpAddr, SocketAddr};
+use std::net::SocketAddr;
 use std::process::ExitCode;
-
-struct Args {
-    ip_address: IpAddr,
-    port: u16,
-}
-
-fn parse_args() -> Result<Args, String> {
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 3 {
-        let executable_name =
-            if args.is_empty() { "recipes" } else { &args[0] };
-        Err(format!("Usage: {} <IP address> <port>", executable_name))
-    } else {
-        let ip_address =
-            args[1].parse::<IpAddr>().map_err(|err| err.to_string())?;
-        let port = args[2].parse::<u16>().map_err(|err| err.to_string())?;
-        Ok(Args { ip_address, port })
-    }
-}
 
 #[tokio::main]
 async fn run_server() -> Result<(), String> {
-    let args = parse_args()?;
+    let config = config::get_config()?;
 
     let app = Router::new()
         .route("/", get(|| async { "Hello, world!" }))
         .nest("/api", api::create_router());
 
-    axum::Server::bind(&SocketAddr::new(args.ip_address, args.port))
-        .serve(app.into_make_service())
-        .await
-        .map_err(|err| err.to_string())
+    axum::Server::bind(&SocketAddr::new(
+        config.server.ip_address,
+        config.server.port,
+    ))
+    .serve(app.into_make_service())
+    .await
+    .map_err(|err| err.to_string())
 }
 
 fn main() -> ExitCode {
