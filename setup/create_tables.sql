9697568397bfaf237fe675aec613fb8591418354
Thomas Smith <thsm@umich.edu>
2023-06-14 23:55:19 -0400

    Finish adding primary key constraints to tables

--- setup/create_tables.sql
+++ setup/create_tables.sql
@@ -7,19 +7,23 @@ CREATE TABLE IF NOT EXISTS ingredients (
 CREATE TABLE IF NOT EXISTS recipes (
   id              INTEGER PRIMARY KEY NOT NULL,
   name            TEXT NOT NULL,
+  hidden          BOOLEAN NOT NULL
 );
 
 -- TODO: Add foreign key constraints
 CREATE TABLE IF NOT EXISTS recipes_ingredients (
   recipe_id       INTEGER NOT NULL,
+  version_id      INTEGER NOT NULL,
   ingredient_id   INTEGER NOT NULL,
   quantity        DOUBLE PRECISION NOT NULL,
-  measurement     TEXT NOT NULL
+  measurement     TEXT NOT NULL,
+  PRIMARY KEY (recipe_id, version_id, ingredient_id)
 );
 
--- TODO: Add primary key constraint for (recipe_id, step_number) if possible
 CREATE TABLE IF NOT EXISTS recipes_instructions (
   recipe_id       INTEGER NOT NULL,
+  version_id      INTEGER NOT NULL,
   step_number     INTEGER NOT NULL,
-  step_text       TEXT NOT NULL
+  step_text       TEXT NOT NULL,
+  PRIMARY KEY (recipe_id, version_id, step_number)
 );
