66ac1474c56ffb4fedb1abf7f3fea830217adb09
Thomas Smith <thsm@umich.edu>
2023-07-09 00:15:15 -0400

    Add recipe version creation timestamp to model

--- src/models/recipeversion.rs
+++ src/models/recipeversion.rs
@@ -1,5 +1,6 @@
 use super::{Ingredient, Model, Ref};
 use crate::database::DBResult;
+use chrono::{offset::Utc, DateTime, NaiveDateTime};
 use log::warn;
 use serde::Serialize;
 use sqlx::{Any, Transaction};
@@ -41,6 +42,7 @@ pub struct Instruction {
 #[derive(Serialize)]
 pub struct RecipeVersion {
     pub id: i64,
+    pub created: DateTime<Utc>,
     pub ingredients: Vec<QuantifiedIngredient>,
     pub instructions: Vec<Instruction>,
 }
@@ -58,6 +60,14 @@ impl Model for RecipeVersion {
         transaction: &mut Transaction<'_, Any>,
         id: Self::ID,
     ) -> DBResult<Self> {
+        let created_secs_since_epoch: i64 = sqlx::query_scalar(
+            "SELECT created FROM recipes_versions WHERE recipe_id = $1 AND version_id = $2"
+        )
+            .bind(id.recipe_id)
+            .bind(id.version_id)
+            .fetch_one(&mut *transaction)
+            .await?;
+
         let ingredients_raw: Vec<(i64, f64, i64)> = sqlx::query_as(
             "SELECT ingredient_id, quantity, measurement FROM recipes_ingredients WHERE recipe_id = $1 AND version_id = $2 ORDER BY list_order"
         )
@@ -69,6 +79,15 @@ impl Model for RecipeVersion {
         let instructions_raw: Vec<String> = sqlx::query_scalar(
             "SELECT step_text FROM recipes_instructions WHERE recipe_id = $1 AND version_id = $2 ORDER BY step_number"
         ).bind(id.recipe_id).bind(id.version_id).fetch_all(transaction).await?;
+
+        let Some(created_naive) =
+            NaiveDateTime::from_timestamp_opt(created_secs_since_epoch, 0)
+        else {
+            return Err(sqlx::Error::Decode(
+                "Internal error: timestamp out-of-range".into(),
+            ));
+        };
+        let created = created_naive.and_utc();
 
         let ingredients = ingredients_raw.into_iter().map(|(ingredient_id, quantity, measurement)| {
             QuantifiedIngredient {
@@ -93,6 +112,7 @@ impl Model for RecipeVersion {
 
         Ok(Self {
             id: id.version_id,
+            created,
             ingredients,
             instructions,
         })
