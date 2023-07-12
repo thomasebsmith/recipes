288f14b63e6b6f45c14ddd89b00452fd50bee215
Thomas Smith <thsm@umich.edu>
2023-07-11 23:47:34 -0400

    Query recipes_versions to find recipe version IDs

--- src/models/recipe.rs
+++ src/models/recipe.rs
@@ -27,7 +27,7 @@ impl Model for Recipe {
         .await?;
 
         let version_ids: Vec<i64> = sqlx::query_scalar(
-            "SELECT DISTINCT version_id FROM recipes_ingredients WHERE recipe_id = $1",
+            "SELECT version_id FROM recipes_versions WHERE recipe_id = $1",
         )
         .bind(id)
         .fetch_all(&mut *transaction)
