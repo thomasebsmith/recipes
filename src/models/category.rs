ff491a84ccf833a18b0a65c86d73a5f967b83852
Thomas Smith <thsm@umich.edu>
2023-09-17 23:48:35 -0400

    Add code to store new categories in the database

--- src/models/category.rs
+++ src/models/category.rs
@@ -14,6 +14,33 @@ pub struct Category {
     pub name: String,
 }
 
+impl Category {
+    // TODO: Use this in the API
+    #[allow(dead_code)]
+    pub async fn store_new(
+        transaction: &mut Transaction<'_, Any>,
+        name: &str,
+    ) -> DBResult<i64> {
+        let last_category_id: i64 =
+            sqlx::query_scalar("SELECT MAX(id) FROM categories")
+                .fetch_one(&mut *transaction)
+                .await?;
+
+        let id = last_category_id + 1;
+
+        sqlx::query(
+            "INSERT INTO ingredients (id, name)
+             VALUES ($1, $2)",
+        )
+        .bind(id)
+        .bind(name)
+        .execute(transaction)
+        .await?;
+
+        Ok(id)
+    }
+}
+
 impl Model for Category {
     type ID = i64;
 
