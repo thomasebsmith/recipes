3a2070df77e2addec5c40704c4b2758646bcfbdc
Thomas Smith <thsm@umich.edu>
2023-09-21 23:50:31 -0400

    Implement route to list categories

--- src/api/routes/categories.rs
+++ src/api/routes/categories.rs
@@ -11,6 +11,36 @@ use crate::api::utils::Error;
 use crate::database::Database;
 use crate::models::{Category, Model};
 
+async fn list_categories(
+    State(database): State<Arc<Database>>,
+) -> Result<Json<Vec<Category>>, Error> {
+    debug!("Listing all categories");
+
+    Ok(Json(
+        database
+            .with_transaction(move |transaction| {
+                Box::pin(async move {
+                    // TODO: Move this elsewhere.
+                    const LISTING_LIMIT: i64 = 1024;
+
+                    let results: Vec<(i64, String)> = sqlx::query_as(
+                        "SELECT id, name FROM categories ORDER BY id LIMIT $1",
+                    )
+                    .bind(LISTING_LIMIT)
+                    .fetch_all(transaction)
+                    .await?;
+
+                    Ok(results
+                        .into_iter()
+                        .map(|(id, name)| Category { id, name })
+                        .collect())
+                })
+            })
+            .await
+            .map_err(Error::from_sqlx)?,
+    ))
+}
+
 async fn get_category(
     State(database): State<Arc<Database>>,
     Path(category_id): Path<i64>,
@@ -31,6 +61,7 @@ async fn get_category(
 
 pub fn create_router(database: Arc<Database>) -> Router {
     Router::new()
+        .route("/", get(list_categories))
         .route("/:category_id", get(get_category))
         .with_state(database)
 }
