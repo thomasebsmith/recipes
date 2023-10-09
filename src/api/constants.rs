498ca921c09c84670c0576f8f2c282a3d78f2e2a
Thomas Smith <thsm@umich.edu>
2023-10-08 23:41:54 -0400

    Add doc comment for LISTING_LIMIT

--- src/api/constants.rs
+++ src/api/constants.rs
@@ -1,1 +1,5 @@
+/// The maximum number of results to retrieve per listing request.
+///
+/// This limit only applies to single requests. It may be exceeded across
+/// multiple requests using pagination.
 pub const LISTING_LIMIT: i64 = 1024;
