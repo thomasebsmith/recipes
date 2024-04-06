0d91df992452983d0c03ed718a5cb3187bbf4771
Thomas Smith <thsm@umich.edu>
2024-04-05 23:59:49 -0400

    Add doc comments to ModelCache

--- src/database/modelcache.rs
+++ src/database/modelcache.rs
@@ -1,8 +1,11 @@
 #[allow(dead_code)]
 pub trait ModelCache {
+    /// The type of ID that is used as a primary key for a model type.
     type Id;
 
+    /// Retrieve an unused ID from the cache.
     fn unused_id() -> Self::Id;
 
+    /// Mark the specified ID as used in the cache.
     fn use_id(id: Self::Id);
 }
